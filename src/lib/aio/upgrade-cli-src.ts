91b1b1e237dd80511e9cdc7cfb704af2a199b9f4
George Kalpakas <kalpakas.g@gmail.com>
2021-12-13 23:34:06 +0200

    fix(aio/upgrade-cli-src): change PR reviewer from Pete to Joey
    Change the requested PR reviewer to Joey, since Pete has left the team
    :'(

--- src/lib/aio/upgrade-cli-src.ts
+++ src/lib/aio/upgrade-cli-src.ts
@@ -34,7 +34,7 @@ export class Upgradelet extends BaseUpgradelet {
   private static readonly AIO_SCRIPT_RE = /^node \S+ ([\da-f]+)$/;
   private static readonly LOCAL_BRANCH_PREFIX = parse(__filename).name;
   private static readonly COMMIT_MESSAGE_PREFIX = 'build(docs-infra): upgrade cli command docs sources to ';
-  private static readonly PR_REVIEWER = 'petebacondarwin';
+  private static readonly PR_REVIEWER = 'josephperrott';
   private static readonly PR_MILESTONE = 'docs-infra-tooling';
   private static readonly PR_LABELS_NEW = [
     'action: merge',
